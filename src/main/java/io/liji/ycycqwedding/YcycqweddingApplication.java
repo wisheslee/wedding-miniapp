baa0a15abe210ec2c47ad9ea165aa6f8974c75ee
liji <liji@ruoshui.me>
2018-08-30 23:37:03 +0800

    增加mapperScan

--- src/main/java/io/liji/ycycqwedding/YcycqweddingApplication.java
+++ src/main/java/io/liji/ycycqwedding/YcycqweddingApplication.java
@@ -1,9 +1,11 @@
 package io.liji.ycycqwedding;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("io.liji.ycycqwedding.dao.mapper")
 public class YcycqweddingApplication {
 
     public static void main(String[] args) {
