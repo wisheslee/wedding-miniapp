40110678a0911e6862f0ff006abef3e528bdb091
liji <liji@ruoshui.me>
2018-09-01 21:28:04 +0800

    测试

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 pwd
 $MAVEN_HOME/bin/mvn clean package
+docker stop ycycqwedding
 docker build -t ycycqwedding .
 docker rmi $(docker images -f "dangling=true" -q)
-docker stop ycycqwedding
 docker run --rm -p 2222:2222 -itd ycycqwedding
