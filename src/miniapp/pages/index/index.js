a6381b33896b33dca0d790e80b90e8ecda48e7dd
liji <liji@ruoshui.me>
2018-09-02 17:01:26 +0800

    fix

--- src/miniapp/pages/index/index.js
+++ src/miniapp/pages/index/index.js
@@ -5,17 +5,9 @@ const app = getApp();
 Page({
   data: {
     userInfo: "",
-    hasUserInfo: false,
+    task1QuestionVisible: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    bgImage: "https://liji-image.oss-cn-hongkong.aliyuncs.com/backgroundImage.jpg?x-oss-process=image/quality,Q_50/auto-orient,1",
-    imageList: [
-      "https://liji-image.oss-cn-hongkong.aliyuncs.com/image0.JPG?x-oss-process=image/quality,Q_50/auto-orient,1",
-      "https://liji-image.oss-cn-hongkong.aliyuncs.com/image1.JPG?x-oss-process=image/quality,Q_50/auto-orient,1",
-      "https://liji-image.oss-cn-hongkong.aliyuncs.com/image2.jpeg?x-oss-process=image/quality,Q_50/auto-orient,1",
-      "https://liji-image.oss-cn-hongkong.aliyuncs.com/image3.jpeg?x-oss-process=image/quality,Q_50/auto-orient,1",
-      "https://liji-image.oss-cn-hongkong.aliyuncs.com/image4.JPG?x-oss-process=image/quality,Q_50/auto-orient,1",
-      "https://liji-image.oss-cn-hongkong.aliyuncs.com/image5.JPG?x-oss-process=image/quality,Q_50/auto-orient,1",
-    ]
+    imageList: ["/img/01.jpeg", "/img/02.jpeg", "/img/03.jpeg", "/img/04.jpeg", "/img/05.jpeg", "/img/06.jpeg"]
   },
   onLoad: function () {
     wx.showLoading();
@@ -31,12 +23,20 @@ Page({
   },
   click(event) {
     const index = event.currentTarget.dataset.index;
+    console.log(111, index)
     switch(index){
       case 0:
-        console.log(22222)
-        wx.navigateTo({"url": "/pages/task1/task1"})
+        console.log(222)
+        this.setData({
+          task1QuestionVisible: true
+        })
+        break;
       default:
-        console.log(11111)
+        wx.showToast({
+          title: '请先扫对应二维码',
+          icon: "none",
+          duration: 2000
+        })
     }
   },
 });
