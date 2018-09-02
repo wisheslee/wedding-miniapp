4574ed08bc29f1e1b44583b81913656e7e3e81cf
liji <liji@ruoshui.me>
2018-09-03 02:06:01 +0800

    测试

--- src/miniapp/app.js
+++ src/miniapp/app.js
@@ -14,29 +14,29 @@ App({
               wx.setStorageSync("userInfo", res.data.data);
               this.globalData.userInfo = res.data.data;
               //检查权限
-    wx.getSetting({
-      success: (res) => {
-        if (res.authSetting['scope.userInfo']) {
-          this.globalData.hasAuth = true;
-          wx.getUserInfo({
-            withCredentials: false,
-            lang: "zh_CN",
-            success: res => {
-              this.globalData.hasAuth = true;
-              console.log("getUserInfo", res)
-              this.globalData.userInfo.name = res.userInfo.nickName;
-              this.globalData.userInfo.avatar = res.userInfo.avatarUrl;
-              wx.setStorageSync("userInfo", this.globalData.userInfo);
-              wx.request({
-                url: this.globalData.url + "/user",
-                method: "POST",
-                data: this.globalData.userInfo
+              wx.getSetting({
+                success: (res) => {
+                  if (res.authSetting['scope.userInfo']) {
+                    this.globalData.hasAuth = true;
+                    wx.getUserInfo({
+                      withCredentials: false,
+                      lang: "zh_CN",
+                      success: res => {
+                        this.globalData.hasAuth = true;
+                        console.log("getUserInfo", res)
+                        this.globalData.userInfo.name = res.userInfo.nickName;
+                        this.globalData.userInfo.avatar = res.userInfo.avatarUrl;
+                        wx.setStorageSync("userInfo", this.globalData.userInfo);
+                        wx.request({
+                          url: this.globalData.url + "/user",
+                          method: "POST",
+                          data: this.globalData.userInfo
+                        })
+                      }
+                    })
+                  }
+                }
               })
-            }
-          })
-        }
-      }
-    })
             }
           })
 
